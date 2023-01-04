2f9bfe22c5b1f3f455ea0500041c41c1c466e2d6
xavier-delfini <91735539+xavier-delfini@users.noreply.github.com>
2023-01-04 10:30:28 +0100

    Finalisation des nombres premiers

---
+++ jour03/Job05/main.py
@@ -0,0 +1,18 @@
+def premier():
+    i = 0
+    while i < 1001:
+        def calcul():
+            j = 2
+            prim = 1
+            while i > j:
+                if i % j == 0:
+                    prim = 0
+                j += 1
+            if prim == 1:
+                print(i)
+
+        calcul()
+        i += 1
+
+
+premier()
