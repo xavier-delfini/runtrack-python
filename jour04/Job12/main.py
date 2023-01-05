9f3d003b6d067bf5dd9367a0e2eef1e1b57572a5
xavier-delfini <91735539+xavier-delfini@users.noreply.github.com>
2023-01-05 13:07:27 +0100

    Finalisation du job 13

--- jour04/Job12/main.py
+++ jour04/Job12/main.py
@@ -1,24 +1,25 @@
 def liste(l):
-    j=0
-    k=2
-    while k>j:
-        i=0
+    j = 0
+    k = 2
+    while k > j:
+        i = 0
         while l[i]:
-            k=2
+            k = 2
 
-            temp1= l[i]
+            temp1 = l[i]
             try:
-                temp2= l[i+1]
+                temp2 = l[i + 1]
                 if temp1 > temp2:
-                    l[i]= temp2
-                    l[i+1]= temp1
+                    l[i] = temp2
+                    l[i + 1] = temp1
             except Exception:
                 break
 
-            i+=1
-            k+=1
-        j+=1
+            i += 1
+            k += 1
+        j += 1
     print(l)
 
 
-liste([9,8,3,6,12,81,42,53,82])
+liste([9, 8, 3, 6, 82, 12, 81, 42, 83, 53, 82])
+liste([3, 7, 1, 3, 8, 12])
---
+++ jour04/Job13/main.py
@@ -0,0 +1,37 @@
+def liste():
+    L = [10, 20, 30, 20, 10, 50, 60, 40, 80, 50, 40]
+
+    def compteur():
+        n = 0
+        i = 1
+        while i:
+            try:
+                L[i]
+                i += 1
+                n += 1
+            except Exception:
+                break
+        return n
+
+    n=compteur()
+    j=1
+    while j<=n:
+        i = 0
+        while i<=n:
+            print ("i")
+            print (i)
+            print ("n")
+            print(n)
+            if i != j and L[i] == L[j]:
+                del L[j]
+                n=compteur()
+
+
+
+
+
+            i+=1
+        j+=1
+    print (L)
+
+liste()
