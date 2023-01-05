4a94c4d77d1cb18dce9cd3a84e707e9cfd996737
xavier-delfini <91735539+xavier-delfini@users.noreply.github.com>
2023-01-05 10:30:28 +0100

    Correction du Job10 jour 04

--- jour04/Job10/main.py
+++ jour04/Job10/main.py
@@ -2,10 +2,10 @@ def liste():
     L = [8, 24, 27, 48, 2, 16, 9, 102, 7, 84, 91]
     n = len(L)
     i=0
-    produit=0
+    produit=1
     while n > i:
         if L[i]>=25 and L[i]<=90:
-            produit= produit + L[i]
+            produit= produit * L[i]
         i+=1
     print(produit)
 
