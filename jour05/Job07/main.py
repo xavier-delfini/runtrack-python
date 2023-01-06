67b7eb1c418233c0e6470109504462ff88039ffe
xavier-delfini <91735539+xavier-delfini@users.noreply.github.com>
2023-01-06 12:56:46 +0100

    Création de la première fonction du job bonus

--- jour05/Job07/main.py
+++ jour05/Job07/main.py
@@ -1,3 +1,25 @@
-def meli_memo()
+def meli_memo():
+    import string
     mot=input("Veuillez renseignez un mot:")
     alphabet = string.ascii_lowercase
+    liste=list(mot)
+    n=len(mot)
+
+    def calcul_mot(valeur):
+        i=0
+        résultat=0
+        while n>i:
+            dump = alphabet.find(valeur[i])+1
+            résultat=dump * (26 ** i) + résultat
+            i+=1
+        return résultat
+    def test_mots:
+        while
+
+
+    print(calcul_mot(mot))
+
+meli_memo()
+
+
+
